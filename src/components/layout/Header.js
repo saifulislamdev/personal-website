691e763972c00b4e5c646e0b58cf173d27eb2254
Saiful Islam <nysaifulislam@gmail.com>
2023-01-14 21:52:12 -0500

    added Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
-  return <div></div>;
+  const getPageName = () => {
+    const pathName = window.location.pathname;
+    const pageName = pathName.split('/')[1];
+    return pageName;
+  };
+
+  const pageName = getPageName();
+
+  return (
+    // <Navbar expand='lg'>
+    <Navbar>
+      <Container>
+        <Navbar.Brand href='/'>Saiful Islam</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse className='justify-content-end'>
+          <Nav variant='pills' activeKey={pageName}>
+            <Nav.Item>
+              <Nav.Link eventKey='projects' href='/projects'>
+                Projects
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey='contact' href='/contact'>
+                Contact
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
